f98b54dcb2d685a3097b2c5322bc33c67d0bcf5c
Glitch (codenation-playlistproject) <none>
2022-02-17 23:26:25 +0000

    👝⬛ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let songs = ["Brittle Bones Nicky", "By the Sword", "Rock Parade", "Devil Trigge
 let images = ["https://images.genius.com/b7773a5bbb51172925d4174fce8aae48.500x500x1.jpg", 
               "https://images.genius.com/5f2866daec88246edf668a0e00c40afe.1000x1000x1.jpg",
               "https://i.ytimg.com/vi/MA6iuIKRzPY/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCmSHnDBjljyfzoLFtcZJltD7k_WQ",
-              "https://static.wikia.nocookie.net/devilmaycry/images/0/0a/DMC5_Devil_Trigger.jpg/revision/latest?cb=20190424163453",
+              "http://pm1.narvii.com/6953/2b8485fcc08ef4375c1a0314933ed7780c145c0dr1-400-400v2_00.jpg",
               "https://upload.wikimedia.org/wikipedia/en/5/5c/Enemy_Imagine_Dragons.jpg",
              ];
 let artists = ["Rare Americans", "iamjakehill", "NAOKI, Arc System Works", "Casey Edwards", "Imagine Dragons"];
@@ -13,8 +13,23 @@ let links = ["https://open.spotify.com/album/3dNESOkTfBWV7R3bf1GPcS",
              "https://open.spotify.com/track/7rjWCCv5bO49a99nsLNhfu",
              "https://open.spotify.com/track/7I8MCiM3A4xvnM6zIiuMZn",
              "https://open.spotify.com/track/1r9xUipOqoNwggBpENDsvJ"];
-function displaySongInfo() {
   // Complete the Day 2 goals inside this function
+function originalList() {
+  songs.forEach(function(songNames) {
+       $(".columnSongs").append(`<li>${songNames}</li>`);
+  });
+  images.forEach(function(images) {
+       $(".columnImages").append(`<img src="${images}">`);
+  });
+  artists.forEach(function(artistNames) {
+       $(".columnArtists").append(`<li>${artistNames}</li>`);
+  });
+  lengths.forEach(function(lengths) {
+       $(".columnLengths").append(`<li>${lengths}</li>`);
+  });
+  links.forEach(function(links) {
+       $(".columnLinks").append(`<li> <a href = "${links}">${links}</a> </li>`);
+  });
 }
 
 function emptySongInfo() {
@@ -32,13 +47,10 @@ function emptySongInfo() {
 function addSongInfo() {
   // Complete Day 3 goals inside this function
 }
-for(let i = 0; i < songs.length; i++) {
-  
-}
-$("#add").click(function () {
+originalList();
+/* $("#add").click(function () {
   emptySongInfo();
   addSongInfo();
   displaySongInfo();
 });
-
-displaySongInfo();
+*/
